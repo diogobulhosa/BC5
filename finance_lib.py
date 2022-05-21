9da84845408abc73090ec7af2b74ae3561c6de97
diogobulhosa <dibulhosa@gmail.com>
2022-05-21 10:16:55 +0100

    lots of updates

--- finance_lib.py
+++ finance_lib.py
@@ -3,14 +3,23 @@ import numpy as np
 import talib as tb
 import yfinance as yf
 
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+import datetime
+from datetime import date
+from datetime import datetime as dt
+from dateutil.relativedelta import *
+from datetime import timedelta
+
+from sklearn.ensemble import RandomForestRegressor
+
+
+
 def df_converter(df): 
     df_sp500 = yf.download('^GSPC', 
-                      start='2017-11-09', 
-                      end='2022-05-08', 
                       progress=False)
-    df_dollar = yf.download('DX=F', 
-                      start='2017-11-09', 
-                      end='2022-05-08', 
+    df_dollar = yf.download('DX=F',  
                       progress=False)
     # clearing dollar and sp500 df
     df_dollar.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
@@ -80,3 +89,108 @@ def df_converter(df):
     result.dropna(inplace=True)
 
     return result 
+# to predict models functions
+def add_days(df, forecast_length):
+    end_point = len(df)
+    df1 = pd.DataFrame(index=range(forecast_length), columns=range(2))
+    df1.columns = ['Close', 'Date']
+    df = df.append(df1)
+    df = df.reset_index(drop=True)
+    x = df.at[end_point - 1, 'Date']
+    x = pd.to_datetime(x, format='%Y-%m-%d')
+    for i in range(forecast_length):
+        df.at[df.index[end_point + i], 'Date'] = x + timedelta(days=1+i)
+        df.at[df.index[end_point + i], 'Close'] = 0
+    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+    df = df.drop(['Date'], axis=1)
+    return df
+
+def forecasting(model,df1, forecast_length):
+    df3 = df1[['Close', 'Date']]
+    df3 = add_days(df3, forecast_length)
+    finaldf = df1.drop('Date', axis=1)
+    finaldf = finaldf.reset_index(drop=True)
+    end_point = len(finaldf)
+    x = end_point - forecast_length
+    finaldf_train = finaldf.loc[:x - 1, :]
+    finaldf_train_x = finaldf_train.loc[:, finaldf_train.columns != 'Close']
+    finaldf_train_y = finaldf_train['Close']
+
+    fit = model.fit(finaldf_train_x, finaldf_train_y)
+    yhat = []
+    end_point = len(finaldf)
+    df3_end = len(df3)
+    for i in range(forecast_length, 0, -1):
+        y = end_point - i
+        inputfile = finaldf.loc[y:end_point, :]
+        inputfile_x = inputfile.loc[:, inputfile.columns != 'Close']
+        pred_set = inputfile_x.head(1)
+        pred = fit.predict(pred_set)
+        df3.at[df3.index[df3_end - i], 'Close'] = pred[0]
+        finaldf = df1.drop('Date', axis=1)
+        finaldf = finaldf.reset_index(drop=True)
+        yhat.append(pred)
+    yhat = np.array(yhat)
+    return yhat
+def predictions(df_coin,model, forecast_lenght = 5, train_lenght = 100,target = 'Close'):
+    """ df_coin must be with date in index,
+        forecast_lenght is the amount of days that we will predict
+        model is the model predefined to use to get our predictions
+        train_length is the amount of days that we will use to train the model
+        target is what we are predicting
+        This will return a graphic that will contain the data from the train set and our predictions
+    """
+    df_coin = df_coin.tail(train_lenght)
+    df_coin.reset_index(inplace=True)
+    df_coin['Date'] = pd.to_datetime(df_coin['Date'], format='%Y-%m-%d')
+
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=df_coin['Date'], y=df_coin[target], 
+                    name='Actual Values', mode='lines',line=dict(color='black')))
+    #Predictions
+    forecast = forecasting(model,df_coin,forecast_lenght)
+    #df that will contain the predictions
+    df_pred = pd.DataFrame(columns=['Date',target])
+    #Adding the predictions to our dataset
+    for day, x in enumerate(forecast):
+        new_row={'Date':df_coin['Date'].max() + timedelta(days=1+day),
+     target:x[0]}
+        df_pred = df_pred.append(new_row, ignore_index=True)
+
+    df_pred['Date'] = pd.to_datetime(df_pred['Date'], format='%Y-%m-%d')
+
+    fig2.add_trace(go.Scatter(x=df_pred['Date'], y=df_pred[target], name='Predictions', mode='lines',line=dict(color='red')))
+    fig2.update_layout(dict(updatemenus=[
+                        dict(
+                        type = "buttons",
+                        direction = "left",
+                        buttons=list([
+                                dict(
+                                args=["visible", "legendonly"],
+                                label="Deselect All",
+                                method="restyle"
+                                ),
+                                dict(
+                                args=["visible", True],
+                                label="Select All",
+                                method="restyle"
+                                )
+                        ]),
+                        pad={"r": 10, "t": 10},
+                        showactive=False,
+                        x=1,
+                        xanchor="right",
+                        y=1.1,
+                        yanchor="top"
+                        ),
+                ]
+        ))
+    return fig2
+
+
+#def choose_model(model): 
+#    if model == 'RF': 
+#       return  RandomForestRegressor(random_state=10,criterion='mae', max_depth=20, max_features='auto', n_estimators=30)
+    
+
+
--- first_test.py
+++ first_test.py
@@ -67,7 +67,7 @@ tab_analysis =  html.Div([
             dcc.Graph(
                 id='graph_price'
             )
-            ], className='box', style={'margin-top': '1%', 'width': '80%', 'margin-left': '2%'}),
+            ], className='box', style={'margin-top': '1%', 'width': '80%', 'margin-left': '1%'}),
     ],className='circ_box',style={'margin-left': '0%'})
 
 ], className='main')
@@ -76,22 +76,65 @@ tab_predictions =  html.Div([
     ## choose coin / target
     html.Div([
         html.Div([
-            html.H4('Choose crypto'),
-            html.Br(), 
-            dcc.Dropdown(
-                id='main_coin_dropdown_pred',
-                value='BTC-USD',
-                multi=False,
-                options=test_list, 
-                style={'color': 'black', 'background-color':'#d3d3d3'}) ,
-            dcc.DatePickerRange(
-                id = 'data_picker_pred',
-                start_date=date(2017, 6, 21),
-                display_format='DD-MM-YYYY',
-                start_date_placeholder_text='DD-MM-YYYY',
-                style={'color': 'black', 'background-color':'#d3d3d3'}
-            )            
-            ], className='box', style={'margin-top': '1%'})
+            html.Div([
+                html.H4('Choose crypto'),
+                html.Br(), 
+                dcc.Dropdown(
+                    id='main_coin_dropdown_pred',
+                    value='BTC-USD',
+                    multi=False,
+                    options=test_list, 
+                    style={'color': 'black', 'background-color':'#d3d3d3'}) ,
+                dcc.DatePickerRange(
+                    id = 'data_picker_pred',
+                    start_date=date(2017, 6, 21),
+                    display_format='DD-MM-YYYY',
+                    start_date_placeholder_text='DD-MM-YYYY',
+                    style={'color': 'black', 'background-color':'#d3d3d3'})            
+                ], className='box', style={'margin-top': '1%'}), # crypo choice over here
+            html.Div([
+                html.H4('Choose Target'), 
+                html.Br(),
+                dbc.Row(
+                    justify="center",
+                    children=[
+                        dbc.ButtonGroup(
+                            style={"text-align": "center"},
+                            children=[
+                                dbc.Button(
+                                    "Open",
+                                    color="primary",
+                                    n_clicks=0,
+                                    id="white_color",
+                                    active=False,
+                                ),
+                                dbc.Button(
+                                    "Close",
+                                    color="primary",
+                                    n_clicks=0,
+                                    id="black_color",
+                                    active=False,
+                        ),],),],),                    
+                ], className='box', style={'margin-top': '1%','width': '60%', 'margin-left': '1%'}) # crypo choice over here
+        ],className='circ_box',style={'margin-left': '0%'}),
+    ## predictions graph
+    html.Div([
+            html.Div([
+                html.H4('Choose crypto'),
+                html.Br(),
+                 #### graph
+                dcc.Graph(
+                    id='graph_pred'
+                )          
+                ], className='box', style={'margin-top': '1%'}), # crypo choice over here
+            html.Div([
+                html.H4('Choose Target'), 
+                html.Br(),                                  
+                ], className='box', style={'margin-top': '1%','width': '60%', 'margin-left': '1%'}) # crypo choice over here
+        ],className='circ_box',style={'margin-left': '0%'})
+    
+
+
     ],style={'margin-left': '0%'}), 
         
 ], className='main')
@@ -110,7 +153,7 @@ app.layout = dbc.Container([
     fluid=True,
 )
 
-################################CALLBACK############################################
+################################CALLBACK - Analysis############################################
 
 @app.callback(
     Output(component_id='graph_price', component_property='figure'),
@@ -121,16 +164,17 @@ app.layout = dbc.Container([
      Input('data_picker', 'end_date')]
 )
 
-################################CALLBACKFUNCTIONCIRCUITS############################
-# recebe os years, retorna o grafico+lista de circuitos available nessa altura
+################################CALLBACKFUNCTIONANLYSIS############################
 def callback_1(coin_name, sec_coin_name, check_list, start_date, end_date):
-    print(coin_name,sec_coin_name, check_list, start_date, end_date)
+    print(coin_name,sec_coin_name, check_list, type(start_date), end_date)
     # create dataset
     df_coin = yf.download(coin_name,
                       progress=False,
     )
 
     df_coin = fb.df_converter(df_coin)
+
+    df_coin
 
     if sec_coin_name: 
        sec_df_coin = yf.download(sec_coin_name, 
@@ -142,8 +186,49 @@ def callback_1(coin_name, sec_coin_name, check_list, start_date, end_date):
 
     # first viz 
     fig = px.line(df_coin, df_coin.index, y=df_coin['Close'])
+    fig.update_layout(
+        template="plotly_dark",
+        plot_bgcolor = 'rgba(0, 0, 0, 0)',
+        paper_bgcolor = 'rgba(0, 0, 0, 0)',
+        font_color="white",
+        font_size= 15
+    )
 
     return fig
+###################################################################################################################
+################################CALLBACK - Predictions############################################
+
+@app.callback(
+    Output(component_id='graph_pred', component_property='figure'),
+    [Input('main_coin_dropdown_pred', 'value'),
+     Input('data_picker_pred', 'start_date'),
+     Input('data_picker_pred', 'end_date')]
+)
+
+################################CALLBACKFUNCTIONPREDICTIONS############################
+def callback_2(coin_name, start_date_pred, end_date_pred):
+    print('oi')
+    print(coin_name, type(start_date_pred), end_date_pred)
+    # create dataset
+    df_coin = yf.download(coin_name,
+                      progress=False,
+    )
+
+    df_coin = fb.df_converter(df_coin)
+    #model = fb.choose_m    odel('RF')
+
+    #fig2 = fb.predictions(df_coin,model, 5,100,'Close')
+    
+       # first viz 
+    fig2 = px.line(df_coin, df_coin.index, y=df_coin['Close'])
+    fig2.update_layout(
+        template="plotly_dark",
+        plot_bgcolor = 'rgba(0, 0, 0, 0)',
+        paper_bgcolor = 'rgba(0, 0, 0, 0)',
+        font_color="white",
+        font_size= 15
+    )
+    return fig2
 ###################################################################################################################
 if __name__ == '__main__':
     app.run_server(debug=True)
